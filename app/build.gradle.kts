05de2e362b47b94fef1d67b001ebd00c86cff9f4
Rishabh <meet.rishabh22@gmail.com>
2026-06-22 22:08:32 +0530

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.rishabh.codexapplication"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
